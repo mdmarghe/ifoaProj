5a96d1ebd58b9dd35d2ba54e95da66ef8685db15
mdmarghe <margheritanuccio98@gmail.com>
2023-12-16 18:14:11 +0100

    pd

--- NLP.py
+++ NLP.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import spacy
-
+import re
 
 sentiment_lexicon = {
     "excellent": 3.0,
@@ -48,13 +48,63 @@ sentiment_lexicon = {
     "tasteless": -1.0,
     "disgust": -2.0,
     "repulsive": -2.5,
-    
+    "eccellente": 3.0,
+    "meraviglioso": 2.5,
+    "fantastico": 2.5,
+    "straordinario": 2.5,
+    "ottimo": 2.0,
+    "buono": 1.5,
+    "positivo": 1.5,
+    "impressionante": 2.5,
+    "superbo": 2.0,
+    "eccezionale": 2.0,
+    "terrificante": 2.0,
+    "delizioso": 2.0,
+    "gustoso": 1.5,
+    "delizioso": 1.5,
+    "soddisfacente": 1.5,
+    "piacevole": 1.5,
+    "amore": 2.0,
+    "mi piace": 1.5,
+    "godere": 1.5,
+    "piacevole": 1.0,
+    "neutro": 0.0,
+    "medio": 0.0,
+    "mediocre": -1.0,
+    "deludente": -1.5,
+    "cattivo": -2.0,
+    "orribile": -2.5,
+    "terribile": -2.5,
+    "atroce": -2.5,
+    "non appetitoso": -1.5,
+    "immangiabile": -2.0,
+    "disgustoso": -2.5,
+    "odio": -2.5,
+    "ripugnanza": -2.5,
+    "disprezzo": -2.5,
+    "abborraccio": -2.5,
+    "sgradevole": -1.5,
+    "spiacevole": -1.5,
+    "non appetitoso": -1.5,
+    "insipido": -1.0,
+    "senza sapore": -1.0,
+    "disgusto": -2.0,
+    "ripugnante": -2.5,
+    "mosca":-3,
+
+
 }
 
 nlp=spacy.load("en_core_web_md")
 
+pattern = re.compile(r'\b(?:' + '|'.join(map(re.escape, sentiment_lexicon.keys())) + r')\b', flags=re.IGNORECASE)
 
 def analyze_sentiment(text):
+    matches = pattern.findall(text)
+    sentiment_score = sum(sentiment_lexicon[match.lower()] for match in matches)
+    return sentiment_score
+    
+#def analyze_sentiment(text):
     doc = nlp(text)
     sentiment_score = 0.0
 
